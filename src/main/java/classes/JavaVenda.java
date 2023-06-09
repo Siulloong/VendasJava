629da99b9c611deb2ecfeddd963692ae360c5ab7
EDERSON <76922877+Siulloong@users.noreply.github.com>
2023-06-09 01:58:47 -0300

    teste

---
+++ src/main/java/classes/JavaVenda.java
@@ -0,0 +1,12 @@
+package classes;
+
+import formularios.frmLogin;
+
+public class JavaVenda {
+
+    public static void main(String[] args) {
+        frmLogin miLogin = new frmLogin();
+        miLogin.setLocationRelativeTo(null); 
+        miLogin.setVisible(true);
+    }
+}
